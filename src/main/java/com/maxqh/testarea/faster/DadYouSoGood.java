e8dd74fb80dfa83722a0bc9b02e2f4da2c98e28b
MAX QH <cosp4321@gmail.com>
2018-03-14 15:29:58 +0800

    修改注释

--- src/main/java/com/maxqh/testarea/faster/DadYouSoGood.java
+++ src/main/java/com/maxqh/testarea/faster/DadYouSoGood.java
@@ -1,7 +1,11 @@
 package com.maxqh.testarea.faster;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author:qianhao
@@ -11,19 +15,30 @@ import java.util.concurrent.Future;
  */
 public class DadYouSoGood {
     /**
-     * 一次最多处理50万数据
+     * 一次最多处理的数据量
      */
-    public static final Integer MAX_NUM = 50000000;
+    public static final Integer MAX_NUM = 25000000;
 
     /**
      * 要运算总数
      */
     public static Integer TOTOAL_NUM = 100000000;
 
+    private static final ExecutorService threadPool = new ThreadPoolExecutor(
+            Runtime.getRuntime().availableProcessors() + 1, Runtime.getRuntime().availableProcessors() * 2, 60L,
+            TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
     public static void main(String[] args) throws Exception {
-//         methodA();
-         methodB();
-//         methodC();
+        methodA();
+
+        Thread.sleep(1000);
+        methodB();
+
+        Thread.sleep(1000);
+        methodC();
+
+        Thread.sleep(1000);
+        methodD();
     }
 
     /**
@@ -70,6 +85,37 @@ public class DadYouSoGood {
         SubCallable callable = new SubCallable(0, TOTOAL_NUM);
         Long total = callable.call();
         Long endTime = System.currentTimeMillis();
-        System.out.println(String.format("多线程计算： total num=[%s], and it costs[%s]ms", total, (endTime - startTime)));
+        System.out
+                .println(String.format("多线程计算： total num=[%s], and it costs[%s]ms \n\r", total, (endTime - startTime)));
+    }
+
+    public static void methodD() throws Exception {
+        Long startTime = System.currentTimeMillis();
+
+        int start = 0;
+        int end = TOTOAL_NUM;
+
+        for (; (end - start) > MAX_NUM;) {
+            SubThread sub = new SubThread(start, start + MAX_NUM);
+            start = start + MAX_NUM + 1;
+            threadPool.submit(sub);
+        }
+
+        if (start < end) {
+            SubThread sub = new SubThread(start, end);
+            threadPool.submit(sub);
+        }
+
+        threadPool.shutdown();
+
+        Long endTime;
+        while (true) {
+            if (threadPool.isTerminated()) {
+                endTime = System.currentTimeMillis();
+                System.out.println(String.format("多线程计算： totalNum=[%s], and it costs[%s]ms", SubThread.totalSum,
+                        (endTime - startTime)));
+                break;
+            }
+        }
     }
 }
