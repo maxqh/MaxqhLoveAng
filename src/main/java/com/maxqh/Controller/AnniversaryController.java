4106cb8fe95bae356346fd5ff6a1143ed404b4b9
MAX QH <cosp4321@gmail.com>
2018-03-20 19:56:07 +0800

    bugfixing

--- src/main/java/com/maxqh/Controller/AnniversaryController.java
+++ src/main/java/com/maxqh/Controller/AnniversaryController.java
@@ -1,8 +1,7 @@
 package com.maxqh.Controller;
 
-import java.util.Date;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maxqh.biz.manager.enums.EnumAnniversaries;
@@ -14,14 +13,14 @@ import com.maxqh.biz.manager.utils.DateUtils;
  * @Date:2018/3/11
  * @Version:1.0
  */
-@RestController(value = "/momdad")
+@RestController
+@RequestMapping(value = "/momdad")
 public class AnniversaryController {
     @GetMapping(value = "/days")
-    public Integer days(){
+    public Long days(){
         String gettingTogetherDayStr = EnumAnniversaries.MOM_DAD_GETTING_TOGETHER.getValue();
-        String nowDateStr = DateUtils.getYmdDateString(new Date());
 
-        Integer days = Integer.valueOf(nowDateStr) - Integer.valueOf(gettingTogetherDayStr);
+        Long days = DateUtils.getDaysFromNow(gettingTogetherDayStr);
         return days;
     }
 
--- src/main/java/com/maxqh/biz/manager/utils/DateUtils.java
+++ src/main/java/com/maxqh/biz/manager/utils/DateUtils.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 /**
  * @Author:qianhao
@@ -21,6 +22,13 @@ public class DateUtils {
     public static final String yyyyMMdd = "yyyyMMdd";
 
     /**
+     * 一天 = 60 * 60 * 24 = 86400 秒
+     */
+    public static final Integer DAY_TO_SECONDS = 60 * 60 * 24;
+
+    private static final SimpleDateFormat SHORT_DATEFORMAT = new SimpleDateFormat(yyyyMMdd);
+
+    /**
      * 用yyyyMMdd来格式化日期
      *
      * @author qinying
@@ -28,7 +36,52 @@ public class DateUtils {
      * @return String
      */
     public static String getYmdDateString(Date d) {
-        SimpleDateFormat destsmf = new SimpleDateFormat(yyyyMMdd);
-        return destsmf.format(d);
+        return SHORT_DATEFORMAT.format(d);
+    }
+
+    /**
+     * 计算两个日期比较
+     *
+     * @param date1
+     * @param date2
+     * @return Long
+     */
+    public static Long getDaysBetween(Date date1, Date date2) {
+        Long d1 = date1.getTime() / 1000;
+        Long d2 = date2.getTime() / 1000;
+        return (d2 - d1) / DAY_TO_SECONDS;
+    }
+
+    public static Long getDaysFromNow(String dateStr) {
+        Date minuend = getYmdStringToDate(dateStr);
+        Date subtrahend = getYmdStringToDate(getYmdDate());
+        return getDaysBetween(minuend, subtrahend);
+    }
+
+    /**
+     * 用 yyyyMMdd 来格式化日期
+     *
+     * @param d
+     * @return String
+     * @author qinying
+     */
+    public static Date getYmdStringToDate(String d) {
+        if (StringUtils.isEmpty(d)) {
+            return null;
+        }
+        try {
+            return SHORT_DATEFORMAT.parse(d);
+        } catch (Exception e) {
+            LOG.error("解析日期格式出错，d={}, exceptionInfo={}", d, e);
+        }
+        return null;
+    }
+
+    /**
+     * yyyy-MM-dd 当前日期
+     *
+     */
+    public static String getYmdDate() {
+        return SHORT_DATEFORMAT.format(new Date());
     }
 }
