7afc73ca8d567d0b47a25d518eb2dac32489f731
MAX QH <cosp4321@gmail.com>
2019-04-04 17:15:25 +0800

    单链表

---
+++ src/main/java/com/maxqh/testarea/leetcode/linkedlist/ListNode.java
@@ -0,0 +1,16 @@
+package com.maxqh.testarea.leetcode.linkedlist;
+
+/**
+ * @Author:qianhao
+ * @Description: 链表
+ * @Date:2019/4/4
+ * @Version:1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
