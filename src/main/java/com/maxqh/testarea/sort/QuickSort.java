c44a3c91291b3c3092d56af7f2df9c99912c0348
qianhao <qianhao@guahao.com>
2019-12-31 19:35:09 +0800

    快排

---
+++ src/main/java/com/maxqh/testarea/sort/QuickSort.java
@@ -0,0 +1,53 @@
+package com.maxqh.testarea.sort;
+
+/**
+ * QuickSort
+ *
+ * @program: 快速排序
+ * @description:
+ * @author: qianhao
+ * @create: 2019-12-23 10:43
+ */
+public class QuickSort {
+
+    public static Integer[] quickSort(Integer[] param) {
+        if (param == null || param.length <= 1) {
+            return param;
+        }
+
+        helper(0, param.length - 1, param);
+        return param;
+    }
+
+    private static void helper(Integer start, Integer end, Integer[] param) {
+        int k = param[start];
+        Integer middle = start;
+        for (int i = middle + 1; i <= end; i++) {
+            if (param[i] < k) {
+                int temp = param[i];
+                param[i] = param[middle];
+                param[middle] = temp;
+
+                middle++;
+                temp = param[i];
+                param[i] = param[middle];
+                param[middle] = temp;
+            }
+        }
+
+        if (middle - 1 > start) {
+            helper(start, middle - 1, param);
+        }
+
+        if (end > middle + 1) {
+            helper(middle + 1, end, param);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] result = quickSort(new Integer[] { 12, 3, 41, 5, 6, 2, 1, 132, 0, 132, 131,1232 });
+        for (Integer i = 0; i < result.length; i++) {
+            System.out.println(String.join(",", i.toString(), result[i].toString()));
+        }
+    }
+}
