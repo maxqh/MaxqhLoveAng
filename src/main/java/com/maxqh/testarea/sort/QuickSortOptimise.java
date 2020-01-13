6e384341ca431850bd500380df47e06a03d0cd57
qianhao <qianhao@guahao.com>
2020-01-13 14:22:18 +0800

    平方

--- src/main/java/com/maxqh/testarea/sort/QuickSortOptimise.java
+++ src/main/java/com/maxqh/testarea/sort/QuickSortOptimise.java
@@ -12,25 +12,9 @@ import java.util.stream.Collectors;
  * @author: qianhao
  * @create: 2019-12-23 10:43
  */
-public class QuickSort {
+public class QuickSortOptimise {
 
-    public static int[] quickSort(int[] param) {
-        if (param == null || param.length <= 1) {
-            return param;
-        }
-
-        helper(0, param.length - 1, param);
-        return param;
-    }
-
-    /**
-     * 执行用时 : 20 ms , 在所有 Java 提交中击败了 31.87% 的用户 内存消耗 : 44.3 MB , 在所有 Java 提交中击败了
-     * 100.00% 的用户
-     * 
-     * @param param
-     * @return
-     */
-    public static List<Integer> maxqh(int[] param) {
+    public List<Integer> quickSort(int[] param) {
         if (param == null) {
             return null;
         }
@@ -43,22 +27,25 @@ public class QuickSort {
         return Arrays.stream(param).boxed().collect(Collectors.toList());
     }
 
+
+
     private static void helper(Integer start, Integer end, int[] param) {
-        int k = param[start];
+        int pivot = param[end];
         Integer middle = start;
         int temp;
-        for (int i = middle + 1; i <= end; i++) {
-            if (param[i] < k) {
+        for (int i = start; i <= end; i++) {
+            if (param[i] < pivot) {
                 temp = param[i];
                 param[i] = param[middle];
                 param[middle] = temp;
 
                 middle++;
-                temp = param[i];
-                param[i] = param[middle];
-                param[middle] = temp;
             }
         }
+
+        temp = param[middle];
+        param[middle] = param[end];
+        param[end] = temp;
 
         if (middle - 1 > start) {
             helper(start, middle - 1, param);
@@ -70,9 +57,9 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
-        int[] result = quickSort(new int[] { 12, 3, 41, 5, 6, 2, 1, 132, 0, 132, 131, 1232 });
-        for (Integer i = 0; i < result.length; i++) {
-            System.out.println(String.join(",", i.toString(), String.valueOf(result[i])));
-        }
+//        Integer[] result = quickSort(new Integer[] { 12, 3, 41, 5, 6, 2, 1, 132, 0, 132, 131,1232 });
+//        for (Integer i = 0; i < result.length; i++) {
+//            System.out.println(String.join(",", i.toString(), result[i].toString()));
+//        }
     }
 }
