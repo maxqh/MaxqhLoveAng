cf651d78a1a250e32f060a40bb848969836bde9f
qianhao <qianhao@guahao.com>
2020-03-18 18:21:40 +0800

    commit

---
+++ src/main/java/com/maxqh/testarea/leetcode/string/MultiplyOptimize.java
@@ -0,0 +1,33 @@
+package com.maxqh.testarea.leetcode.string;
+
+/**
+ * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+ * 
+ * 示例 1:
+ * 
+ * 输入: num1 = "2", num2 = "3" 输出: "6" 示例 2:
+ * 
+ * 输入: num1 = "123", num2 = "456" 输出: "56088"
+ * 
+ * 
+ * @program: maxqh_love_ang
+ * @description:
+ * @author: qianhao
+ * @create: 2020-03-16 17:37
+ */
+public class MultiplyOptimize {
+    /**
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public String multiply(String num1, String num2) {
+        if (num1 == null || num2 == null) {
+            return "0";
+        }
+
+        return null;
+    }
+
+}
