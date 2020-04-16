c919b7b96d692ce591e3a234f72bd3cbfa22dc5a
qianhao <qianhao@guahao.com>
2020-04-16 17:34:33 +0800

    commit

--- src/main/java/com/maxqh/testarea/leetcode/array/FindKthLargest.java
+++ src/main/java/com/maxqh/testarea/leetcode/array/FindKthLargest.java
@@ -1,5 +1,8 @@
 package com.maxqh.testarea.leetcode.array;
 
+import com.maxqh.testarea.leetcode.Heap.HeapPriority;
+import com.maxqh.testarea.sort.QuickSort;
+
 import java.util.PriorityQueue;
 
 /**
@@ -11,7 +14,15 @@ import java.util.PriorityQueue;
  * @create: 2019-11-13 10:24
  */
 public class FindKthLargest {
-    public int findKthLargest(int[] nums, int k) {
+    /**
+     * 执行结果： 通过 显示详情 执行用时 : 6 ms , 在所有 Java 提交中击败了 61.33% 的用户 内存消耗 : 40.5 MB , 在所有
+     * Java 提交中击败了 6.12% 的用户
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
         for (Integer item : nums) {
             add(minHeap, item, k);
@@ -20,7 +31,7 @@ public class FindKthLargest {
         return minHeap.peek();
     }
 
-    private void add(PriorityQueue<Integer> minHeap, Integer item, int k) {
+    private static void add(PriorityQueue<Integer> minHeap, Integer item, int k) {
         if (minHeap.size() < k) {
             minHeap.add(item);
         } else if (minHeap.peek() < item) {
@@ -29,5 +40,55 @@ public class FindKthLargest {
         }
     }
 
-    // TODO 实现PriorityQueue小顶堆
+    /**
+     * 执行结果： 通过 显示详情 执行用时 : 5 ms , 在所有 Java 提交中击败了 65.13% 的用户 内存消耗 : 40.4 MB , 在所有
+     * Java 提交中击败了 6.12% 的用户
+     * 
+     * @param nums
+     * @param k
+     * @return
+     * @throws Exception
+     */
+    public static int findKthLargestOptimize(int[] nums, int k) throws Exception {
+        HeapPriority<Integer> minHeap = new HeapPriority<>(k);
+
+        for (Integer item : nums) {
+            add(minHeap, item, k);
+        }
+
+        return minHeap.peek();
+    }
+
+    private static void add(HeapPriority<Integer> minHeap, Integer item, int k) throws Exception {
+        if (minHeap.size() < k) {
+            minHeap.add(item);
+        } else if (minHeap.peek() < item) {
+            minHeap.remove(0);
+            minHeap.add(item);
+        }
+    }
+
+    /**
+     * 执行用时：81 ms 内存消耗：40.8 MB
+     *
+     * 击败了0%的用户
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int findKthLargestVoilence(int[] nums, int k) {
+        if (nums == null || k > nums.length) {
+            return 0;
+        }
+
+        QuickSort.fastSort(nums, 0, nums.length - 1);
+
+        return nums[nums.length - k];
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] param = {};
+        System.out.println(findKthLargestOptimize(param, 918));
+    }
 }
