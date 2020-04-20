1aff545f0b1530009096dbb71eadd718c4e33ae9
qianhao <qianhao@guahao.com>
2020-04-20 19:09:30 +0800

    commit

---
+++ src/main/java/com/maxqh/testarea/leetcode/array/Merge.java
@@ -0,0 +1,90 @@
+package com.maxqh.testarea.leetcode.array;
+
+import com.maxqh.testarea.sort.QuickSortOptimise;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * 合并区间
+ *
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [[1,3],[2,6],[8,10],[15,18]] 输出: [[1,6],[8,10],[15,18]] 解释: 区间 [1,3] 和
+ * [2,6] 重叠, 将它们合并为 [1,6]. 示例 2:
+ * 
+ * 输入: [[1,4],[4,5]] 输出: [[1,5]] 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * 
+ *
+ * @program: maxqh_love_ang
+ * @description:
+ * @author: qianhao
+ * @create: 2020-04-20 10:57
+ */
+
+public class Merge {
+
+    public int[][] mergeVoilence(int[][] intervals) {
+        int[][] result = new int[intervals.length][2];
+
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
+
+        for (int i = 0; i < intervals.length; i++) {
+
+        }
+
+        return null;
+    }
+
+    /**
+     * 执行结果： 通过
+     * 
+     * 显示详情 执行用时 : 7 ms , 在所有 Java 提交中击败了 86.79% 的用户 内存消耗 : 42.6 MB ,
+     * 
+     * 在所有 Java 提交中击败了 35.61% 的用户
+     *
+     * @param intervals
+     * @return
+     */
+    public static int[][] merge(int[][] intervals) {
+        int[][] result = new int[intervals.length][2];
+
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
+
+        QuickSortOptimise.quickSortDoubleArray(intervals);
+
+        int indexI = 0;
+        result[indexI][0] = intervals[0][0];
+        result[indexI][1] = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (result[indexI][1] < intervals[i][0]) {
+                indexI++;
+                result[indexI][0] = intervals[i][0];
+                result[indexI][1] = intervals[i][1];
+                continue;
+            }
+
+            result[indexI][1] = Math.max(result[indexI][1], intervals[i][1]);
+            continue;
+        }
+
+        return Arrays.copyOfRange(result, 0, indexI + 1);
+    }
+
+    public static void main(String[] args) {
+        // int[][] param = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
+        int[][] param = { { 1, 4 }, { 0, 0 } };
+        int[][] result = merge(param);
+        for (int[] item : result) {
+            System.out.println("x=" + item[0] + ",y=" + item[1]);
+        }
+
+    }
+}
--- src/main/java/com/maxqh/testarea/sort/QuickSortOptimise.java
+++ src/main/java/com/maxqh/testarea/sort/QuickSortOptimise.java
@@ -27,8 +27,6 @@ public class QuickSortOptimise {
         return Arrays.stream(param).boxed().collect(Collectors.toList());
     }
 
-
-
     private static void helper(Integer start, Integer end, int[] param) {
         int pivot = param[end];
         Integer middle = start;
@@ -56,10 +54,77 @@ public class QuickSortOptimise {
         }
     }
 
+    public static int[][] quickSortDoubleArray(int[][] param) {
+        if (param == null || param.length <= 1) {
+            return param;
+        }
+
+        helperDoubleArray(0, param.length - 1, param);
+        return param;
+    }
+
+    private static void helperDoubleArray(int start, int end, int[][] param) {
+        int pivot = param[end][0];
+        Integer middle = start;
+        int tempX;
+        int tempY;
+        for (int i = start; i <= end; i++) {
+            if (param[i][0] < pivot) {
+                tempX = param[i][0];
+                param[i][0] = param[middle][0];
+                param[middle][0] = tempX;
+
+                tempY = param[i][1];
+                param[i][1] = param[middle][1];
+                param[middle][1] = tempY;
+
+                middle++;
+            }
+        }
+
+        tempX = param[middle][0];
+        param[middle][0] = param[end][0];
+        param[end][0] = tempX;
+
+        tempY = param[middle][1];
+        param[middle][1] = param[end][1];
+        param[end][1] = tempY;
+
+        if (middle - 1 > start) {
+            helperDoubleArray(start, middle - 1, param);
+        }
+
+        if (end > middle + 1) {
+            helperDoubleArray(middle + 1, end, param);
+        }
+    }
+
     public static void main(String[] args) {
-//        Integer[] result = quickSort(new Integer[] { 12, 3, 41, 5, 6, 2, 1, 132, 0, 132, 131,1232 });
-//        for (Integer i = 0; i < result.length; i++) {
-//            System.out.println(String.join(",", i.toString(), result[i].toString()));
-//        }
+        // Integer[] result = quickSort(new Integer[] { 12, 3, 41, 5, 6, 2, 1, 132, 0,
+        // 132, 131,1232 });
+        // for (Integer i = 0; i < result.length; i++) {
+        // System.out.println(String.join(",", i.toString(), result[i].toString()));
+        // }
+
+        int[][] param = new int[5][2];
+        param[0][0] = 1;
+        param[0][1] = 222;
+
+        param[1][0] = 1;
+        param[1][1] = 2;
+
+        param[2][0] = 12;
+        param[2][1] = 2;
+
+        param[3][0] = 3;
+        param[3][1] = 2;
+
+        param[4][0] = 132;
+        param[4][1] = 2;
+
+        int[][] result = quickSortDoubleArray(param);
+        for (int[] item : result) {
+            System.out.println(item[0]);
+        }
     }
 }
